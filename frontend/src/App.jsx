56f2dd0c343aef4d96e1773cbc8eff2eae2a1675
valon <valonnura888@outlook.com>
2026-05-02 17:07:30 +0200

    Shto React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/landing/LandingPage";
+import Login from "./pages/auth/Login";
+import Cart from "./pages/cart/Cart";
 
 function App() {
   return (
-    <>
-      <LandingPage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
