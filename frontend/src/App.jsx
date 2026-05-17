06ca2788ca743f96dddfe6ec8fd201afef213886
valo0n <valonnura888@outlook.com>
2026-05-17 22:31:57 +0200

    Shtova rrugen per faqen e checkout-it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/landing/LandingPage";
 import Login from "./pages/auth/Login";
 import Cart from "./pages/cart/Cart";
+import Checkout from "./pages/checkout/Checkout";
 import Wishlist from "./pages/wishlist/Wishlist";
 import Shop from "./pages/shop/Shop";
 import AdminLayout from "./pages/admin/AdminLayout";
@@ -23,6 +24,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
         <Route path="/wishlist" element={<Wishlist />} />
         <Route path="/shop" element={<Shop />} />
         <Route
